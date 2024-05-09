36f35813af50d86ac4e353307ccd4b5779d402a2
salvatorebriga <salvo.z2302@gmail.com>
2024-05-09 21:01:39 +0200

    added storeVariables function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 'use strict'
 
+let num = 0;
+
+let op = '';
+
 addNumber();
+storeVariables();
 
 function addNumber(){
 
@@ -11,6 +16,25 @@ function addNumber(){
     for(let i = 0; i < numbersLenght; i++){
         numbers[i].addEventListener('click', function(){
             document.getElementById('display').innerHTML += numbers[i].value;
+        })
+    }
+}
+
+function storeVariables(){
+    const operators = document.querySelectorAll('.operators');
+
+    let operatorsLenght = operators.length;
+
+    for(let i = 0; i < operatorsLenght; i++){
+        operators[i].addEventListener('click', function(){
+
+            num = document.getElementById('display').innerHTML;
+
+            num = parseInt(num);
+
+            op = operators[i].value;
+
+            document.getElementById('display').innerHTML = "";
         })
     }
 }
